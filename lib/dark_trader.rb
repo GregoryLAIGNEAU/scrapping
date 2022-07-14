b5adccfcca86d3892636af256a603fcdf4ab3049
Gregory <gregory.laigneau@protonmail.com>
2022-07-14 20:36:17 +0200

    First commit

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -7,10 +7,10 @@ def scraper
     parsed_page = Nokogiri::HTML(URI.open('https://coinmarketcap.com/all/views/all/'))
     names = []
     prices = []
-    crypto_names = parsed_page.css('.cmc-table-row')
-    crypto_names.each do |crypto_name|
-        names.push(crypto_name.css('a.cmc-link')[0]&.text)
-        prices.push(crypto_name.css('a.cmc-link')[2]&.text)
+    cryptocurrency_names = parsed_page.css('.cmc-table-row')
+    cryptocurrency_names.each do |crypto_name|
+        names << (crypto_name.css('a.cmc-link')[0]&.text)
+        prices << (crypto_name.css('a.cmc-link')[2]&.text)
     end
     print data = names.zip(prices).to_h
 end
