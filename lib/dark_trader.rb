a95bca300e87899d7a3f0b0efd036a699f89d70c
Gregory <gregory.laigneau@protonmail.com>
2022-07-15 09:45:44 +0200

    First commit

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -7,10 +7,10 @@ def dark_trader
     parsed_page = Nokogiri::HTML(URI.open('https://coinmarketcap.com/all/views/all/'))
     names = []
     prices = []
-    cryptocurrency_names = parsed_page.css('.cmc-table-row')
+    cryptocurrency_names = parsed_page.css('.cmc-table-row')[(0..19)]
     cryptocurrency_names.each do |crypto_name|
         names << (crypto_name.css('a.cmc-link')[0]&.text)
-        prices << (crypto_name.css('a.cmc-link')[2]&.text)
+        prices << (crypto_name.css('a.cmc-link')[2]&.text[1..-1])
     end
     print data = names.zip(prices).to_h
 end
--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -3,14 +3,6 @@ require 'open-uri'
 require 'nokogiri'
 require 'byebug'
 
-
-# def get_townhall_email
-#     url = "http://annuaire-des-mairies.com/95/ableiges.html"
-#     parsed_page = Nokogiri::HTML(URI.open(url))
-#     emails = parsed_page.css("main.page-content section.well.section-border:nth-child(2) div.container table.table.table-striped.table-mobile.mobile-primary.round-small tbody:nth-child(3) tr.txt-primary.tr-last:nth-child(4) > td:nth-child(2)").text
-#     emails = Array.new
-# end
-
 def get_townhall_urls
     parsed_page = Nokogiri::HTML(URI.open('http://annuaire-des-mairies.com/val-d-oise.html'))
     cities = parsed_page.css('a.lientxt')
@@ -18,7 +10,18 @@ def get_townhall_urls
     for i in 0...cities.length
         townhall_url.push("http://annuaire-des-mairies.com#{cities[i]['href'].delete_prefix('.')}")
     end
-    print townhall_url
+    townhall_url
 end
 
+def get_townhall_email(townhall_url)
+    email = []
+    for i in 0...townhall_url.length
+        parsed_page = Nokogiri::HTML(URI.open(townhall_url[i]))
+        email.push(parsed_page.css("main.page-content section.well.section-border:nth-child(2) div.container table.table.table-striped.table-mobile.mobile-primary.round-small tbody:nth-child(3) tr.txt-primary.tr-last:nth-child(4) > td:nth-child(2)").text)
+        print email
+    end
+end
+
+townhall_url = get_townhall_urls
 get_townhall_urls
+get_townhall_email(townhall_url)
